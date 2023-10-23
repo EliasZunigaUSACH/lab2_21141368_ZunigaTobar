d04a273e890821e7573871bbbf073cc0f6182e9e
EliasZunigaUSACH <elias.zuniga.t@usach.cl>
2023-10-23 02:12:46 -0300

    Lab 2
    Archivos txt leeme, repositorio y autoevaluación creados.
    Carpeta "Código fuente" creada, por el momento de crearon los archivos .pl system, flow y option.

---
+++ CódigoFuente_21141368_ZuñigaTobar/tda_option_21141368_ZunigaTobar.pl
@@ -0,0 +1,7 @@
+% PARADIGMAS DE PROGRAMACIÓN LABORATORIO 2
+% Nombre: Elías Zúñiga Tobar
+% RUT: 21.141.368-9
+% Profesor Gonzalo Matrinez
+% TDA OPTION
+
+option(Code, Message, Codelink, InitialFlowCode, Keywords, [Code, Message, Codelink, InitialFlowCode, Keywords]).
---
+++ CódigoFuente_21141368_ZuñigaTobar/tda_system_21141368_ZunigaTobar.pl
@@ -0,0 +1,18 @@
+% PARADIGMAS DE PROGRAMACIÓN LABORATORIO 2
+% Nombre: Elías Zúñiga Tobar
+% RUT: 21.141.368-9
+% Profesor Gonzalo Matrinez
+% TDA SYSTEM
+
+putDateTime(DateTime):-
+    get_time(Time),
+    stamp_date_time(Time, Date, local),
+    format_time(atom(DateTime), "%T %d-%M-%Y", Date).
+
+makeSystem(Name, Members, ConectedUser, InitialChatbotCodeLink, Chatbots, [Name, Members, ConectedUser, InitialChatbotCodeLink, Chatbots]):-
+    putDateTime(Date).
+
+makeSystem(Name, Date, Members, ConectedUser, InitialChatbotCodeLink, Chatbots, [Name, Date, Members, ConectedUser, InitialChatbotCodeLink, Chatbots]).
+
+system(Name, InitialChatbotCodeLink, Chatbots, System):-
+    makeSystem(Name, [], [], "", InitialChatbotCodeLink, Chatbots, System).
