ceab544fdc0c339ce40680256cc4cbffd6979c59
EliasZunigaUSACH <elias.zuniga.t@usach.cl>
2023-10-24 01:22:00 -0300

    Update 24/10/23
    Se arregló el predicado makeSystem para que muestre correctamente la hora y fecha de creación del sistema.
    Se movieron los predicados de pertenencia "pertenece" y "noPertenece" al archivo option a modo que sea posible importarlo a l archivo flow.
    Se planea reescribir algunos predicados involucrados en la función flow que eviten duplicidad.

--- CódigoFuente_21141368_ZuñigaTobar/tda_flow_21141368_ZunigaTobar.pl
+++ CódigoFuente_21141368_ZuñigaTobar/tda_flow_21141368_ZunigaTobar.pl
@@ -4,6 +4,9 @@
 % Profesor Gonzalo Matrinez
 % TDA FLOW
 
+:- use_module(tda_option_21141368_ZunigaTobar).
+
+/*
 pertenece(Elemento, [Elemento, _]).
 
 pertenece(Elemento, [_|Resto]):-
@@ -11,23 +14,35 @@ pertenece(Elemento, [_|Resto]):-
 
 noPertenece(Elemento, Lista):-
     \+ pertenece(Elemento, Lista).
+*/
 
-agregarFlowSinDuplicados([], ListaAcc, ListaAcc):- !.
+getFlowId(Flow, Id):-
+    flow(Id, _, _, Flow).
 
-agregarFlowSinDuplicados([ElementoParaAgregar|RestoParaAgregar], Acc, ListaSalida):-
-    noPertenece(ElementoParaAgregar, Acc),
-    agregarFlowSinDuplicados(RestoParaAgregar, [ElementoParaAgregar|Acc], ListaSalida).
-
-agregarFlowSinDuplicados([ElementoParaAgregar|RestoParaAgregar], Acc, ListaSalida):-
-    pertenece(ElementoParaAgregar, Acc),
-    agregarFlowSinDuplicados(RestoParaAgregar, [ElementoParaAgregar|Acc], ListaSalida).
-
-
-agregarFlow(NewOption, Options, NewOptions).
-
-flow(Id, NameMessage, Options, Flows).
+getFlowsIds([], IdsAcc, IdsAcc):- !.
+getFlowsIds([FlowCabeza|FlowsCola], IdsAcc, IdsSalida):-
+    getFlowId(FlowCabeza, Id),
+    getFlowsIds(FlowsCola, [Id|IdsAcc], IdsSalida).
+/*
+agregarFlowSinDuplicados([], FlowsAcc, FlowsAcc):- !.
+agregarFlowSinDuplicados([FlowAgregar|RestoFlows], FlowsAcc, FlowsSalida):-
+    noPertenece(FlowAgregar, FlowsAcc),
+    agregarFlowSinDuplicados(RestoFlows, [FlowAgregar|FlowsAcc], FlowsSalida).
+agregarFlowSinDuplicados([FlowAgregar|RestoFlows], FlowsAcc, FlowsSalida):-
+    pertenece(FlowAgregar, FlowsAcc),
+    agregarFlowSinDuplicados(RestoFlows, [FlowAgregar|FlowsAcc],
+    FlowsSalida).N
+*/
+agregarFlowSinDuplicados([], FlowsAcc, FlowsAcc):- !.
+agregarFlowSinDuplicados([FlowAgregar|RestoFlows], FlowsAcc, FlowsSalida):-
+    getFlowsIds([FlowAgregar|RestoFlows], Ids),
+    noPertenece([]).
 
 flowAddOption(Flow, NewOption, NewFlow):-
     flow(Id, NameMessage, Options, Flow),
     agregarFlow(NewOption, Options, NewOptions),
     flow(Id, NameMessage, NewOptions, NewFlow).
+
+flow(Id, NameMessage, Options, [Id, NameMessage, Options]).
+
+agregarFlow(NewOption, Options, NewOptions).
--- CódigoFuente_21141368_ZuñigaTobar/tda_option_21141368_ZunigaTobar.pl
+++ CódigoFuente_21141368_ZuñigaTobar/tda_option_21141368_ZunigaTobar.pl
@@ -4,4 +4,24 @@
 % Profesor Gonzalo Matrinez
 % TDA OPTION
 
+:- module(tda_option_21141368_ZunigaTobar, [pertenece/2, noPertenece/2]).
+
+pertenece(Elemento, [Elemento, _]).
+
+pertenece(Elemento, [_|Resto]):-
+    pertenece(Elemento, Resto).
+
+noPertenece(Elemento, Lista):-
+    \+ pertenece(Elemento, Lista).
+
+agregarOptionsSinDuplicados([], ListaAcc, ListaAcc):- !.
+
+agregarOptionsSinDuplicados([OpcionAgregar|RestoOpciones], Acc, ListaSalida):-
+    noPertenece(OpcionAgregar, Acc),
+    agregarOptionsSinDuplicados(RestoOpciones, [OpcionAgregar|Acc], ListaSalida).
+
+agregarOptionsSinDuplicados([OpcionAgregar|RestoOpciones], Acc, ListaSalida):-
+    pertenece(OpcionAgregar, Acc),
+    agregarOptionsSinDuplicados(RestoOpciones, [OpcionAgregar|Acc], ListaSalida).
+
 option(Code, Message, Codelink, InitialFlowCode, Keywords, [Code, Message, Codelink, InitialFlowCode, Keywords]).
